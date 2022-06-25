ab58dedc9cadfe1a933588f557ac59fbe60dc131
blazybless <blazybless@gmail.com>
2022-06-26 00:37:41 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom"
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Header() {
+    return (  
+      <div>
+        <nav className="nav">
+          <img src="./logo192.png" className="nav-logo" />
+          <ul className="nav-items">
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </div>
+    )
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Page() {
+  return (
+    <div>
+      <ol>
+        <li>Its awesome</li>
+        <li>Its the best language ever</li>
+      </ol>
+    </div>
+  )
+}
+
+
+      
+function Title() {
+  return (
+    <div>
+      <h1>Reasons I'm excited to learn React</h1>  
+    </div>
+  )
+}
+
+function Footer() {
+  return (
+    <div>
+      <footer>
+        <p>&copy; 2022 BlazyBless development. All rights reserved.</p>
+      </footer>
+    </div>
+  )
+}
+
+
+ReactDOM.render(
+  <div>
+    <Header />
+    <Title />
+    <Page />
+    <Footer />
+  </div>  
+  , document.getElementById('root'))
